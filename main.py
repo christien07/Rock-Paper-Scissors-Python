865b9bbb3bbc1d626f40801bb36d1a71f0141bce
christien07 <christien0101@protonmail.com>
2022-09-26 21:11:59 -0700

    Fix wrong data type

--- main.py
+++ main.py
@@ -32,14 +32,18 @@ computer_choice = random.randint(0, 2)
 
 user_choice = input("Please enter '0' for Rock, enter '1' for Paper, or enter '2' for Scissors.\n")
 
-if user_choice == 0:
+if user_choice == '0':
     if computer_choice == 0:
         print(f"{rock}\n{rock}\nIt's a tie.")
     elif computer_choice == 1:
         print(f"{rock}\n{paper}\nYou lose. Paper beats rock.")
     elif computer_choice == 2:
         print(f"{rock}\n{scissors}\nYou win! Rock beats scissors.")
-elif user_choice == 1:
+elif user_choice == '1':
     # Do something ...
-elif user_choice == 2:
+    print("0")
+elif user_choice == '2':
     # Do something else ...
+    print("0")
+else:
+    print("Please enter '0', '1', or '2' for your choice.")
